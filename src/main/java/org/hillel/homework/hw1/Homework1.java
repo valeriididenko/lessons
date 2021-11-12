09bac2b4c707bf5b74eb70d8387641a3fc54e458
Valerii <nexus1015@gmail.com>
2021-11-12 20:46:28 +0200

    remove while loop

--- src/main/java/org/hillel/homework/hw1/Homework1.java
+++ src/main/java/org/hillel/homework/hw1/Homework1.java
@@ -3,9 +3,7 @@ package org.hillel.homework.hw1;
 public class Homework1 {
 
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("ddsf");
-        }
+        System.out.println("ddsf");
     }
 
 }
