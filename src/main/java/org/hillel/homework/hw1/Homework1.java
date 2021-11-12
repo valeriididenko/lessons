4d0ebd472dcb658d0d3f9697d8fecd3efc237427
Valerii <nexus1015@gmail.com>
2021-11-12 20:42:07 +0200

    init commit

---
+++ src/main/java/org/hillel/homework/hw1/Homework1.java
@@ -0,0 +1,11 @@
+package org.hillel.homework.hw1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("ddsf");
+        }
+    }
+
+}
