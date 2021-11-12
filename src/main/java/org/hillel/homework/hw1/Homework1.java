b8adfdbb28f603d3f0d9a90596037ec0889f15da
Valerii <nexus1015@gmail.com>
2021-11-12 21:03:16 +0200

    another update

--- src/main/java/org/hillel/homework/hw1/Homework1.java
+++ src/main/java/org/hillel/homework/hw1/Homework1.java
@@ -3,7 +3,7 @@ package org.hillel.homework.hw1;
 public class Homework1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 
 }
